95ff11d2d2ee2b26be024d73d0ab23e6ac8d0a09
KhokhlovArtem <32435687+KhokhlovArtem@users.noreply.github.com>
2025-10-23 00:48:12 +0300

    Update start-dc.sh

--- start-dc.sh
+++ start-dc.sh
@@ -1,14 +1,26 @@
 # Загружаем переменные в текущую сессию
 export $(grep -v '^#' .env | xargs)
 
-# Запускаем
+# Сборка и запуск
+docker-compose up -d --build
+
+# Только запуск
 docker-compose up -d
 
 # Проверить, что appsettings.json правильно сконфигурирован в контейнере
-docker exec intraservice-container cat /var/www/intraservice/appsettings.json
+docker exec intraservice-app cat /var/www/intraservice/appsettings.json
+docker exec intraservice-agent cat /var/www/intraservice/appsettings.json
 
 # Проверить логи на предмет ошибок подключения к БД
-docker logs intraservice-container
+docker logs intraservice-app
+docker logs intraservice-agent
+
+или 
+
+docker-compose logs -f
 
 # Проверить переменные окружения
-docker exec intraservice-container printenv
+docker exec intraservice-app printenv
+docker exec intraservice-agent printenv
+
+
